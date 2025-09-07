e3bdaba316b2af2d799a23a2e5381805f20161eb
IvanPareja1 <igpareja@gmail.com>
2025-09-06 23:22:21 -0500

    Fix bucle

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"8QwsUD2MSdMpk3mtZo350bE4Asm2Y1xn3FFZZbcy9DA=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"7tf/CH3mU+rHYQCl8IW9UX4XqdRskurrmcLrGZaXp6c=\"}"
