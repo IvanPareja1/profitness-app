2fd246314b8e4ab7077a5dca14a9005f86ffdb61
IvanPareja1 <igpareja@gmail.com>
2025-09-06 23:18:03 -0500

    Fix bucle

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.VITE_SUPABASE_URL || 'https://lsfzabwrnjmrmdaaogsi.supabase.co'
-const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxzZnphYndybmptcm1kYWFvZ3NpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NjE1MzEyMywiZXhwIjoyMDcxNzI5MTIzfQ.J0-WgR7aFtlxdRmghgqRgK9XORB6Dc9acsy2OE8uN04'
+const supabaseAnonKey = process.env.VITE_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxzZnphYndybmptcm1kYWFvZ3NpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NjE1MzEyMywiZXhwIjoyMDcxNzI5MTIzfQ.J0-WgR7aFtlxdRmghgqRgK9XORB6Dc9acsy2OE8uN04'
 
 // Verificar que las variables no estén vacías
 if (!supabaseUrl || !supabaseAnonKey) {
