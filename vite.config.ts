6fab2d44a1f89f0a05c8ac4c1397c09f4adb2837
IvanPareja1 <igpareja@gmail.com>
2025-09-09 17:29:16 -0300

    nueva prueba 9

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,9 @@
+echo 'import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
 
-export default {
-  "compilerOptions": {
-    "tsBuildInfoFile": "./node_modules/.tmp/tsconfig.app.tsbuildinfo",
-    "target": "ES2022",
-    "useDefineForClassFields": true,
-    "lib": ["ES2022", "DOM", "DOM.Iterable"],
-    "module": "ESNext",
-    "skipLibCheck": true,
-    "moduleResolution": "bundler",
-    "allowImportingTsExtensions": true,
-    "moduleDetection": "force",
-    "noEmit": true,
-    "jsx": "react-jsx",
-    "strict": true,
-    "noUnusedLocals": false,
-    "noUnusedParameters": false,
-    "noFallthroughCasesInSwitch": true,
-    "noUncheckedSideEffectImports": true
-  },
-  "include": ["src"]
-};
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "out"
+  }
+})' > vite.config.ts
