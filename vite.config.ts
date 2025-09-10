2bab1a104f0b0668b52a547db0aabdba19b35f76
IvanPareja1 <igpareja@gmail.com>
2025-09-09 21:48:40 -0300

    nueva prueba 18.8

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-  },
-  server: {
-    port: 3000,
-  },
-  define: {
-    __BASE_PATH__: JSON.stringify(process.env.BASE_PATH || ''),
+    outDir: 'dist'
   }
 })
