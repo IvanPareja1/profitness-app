8956e9532bf7d5a7b1ee671f15a6be798b923e3d
IvanPareja1 <igpareja@gmail.com>
2025-09-12 11:39:10 -0300

    corregir error de recarga

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,20 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ✅ Cambia esto de './' a '/'
+  base: './',
+  define: {
+    __BASE_PATH__: JSON.stringify(''),
+  },
   build: {
     outDir: 'dist',
     sourcemap: false,
     rollupOptions: {
       output: {
-        // ✅ Elimina manualChunks o simplifícalo
-        manualChunks: undefined,
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          i18n: ['react-i18next', 'i18next'],
+        },
       },
     },
   },
